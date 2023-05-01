d8979ff4d3c326fdad2760756f6c01f11ff9d045
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-01 23:39:24 +0600

    swipper slider worked perfectly

--- src/components/pages/HomePageContent/Header.jsx
+++ src/components/pages/HomePageContent/Header.jsx
@@ -3,22 +3,44 @@
 /* eslint-disable react/style-prop-object */
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/jsx-indent */
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 import bannerImg from '../../../assets/banner.jpg';
 import Left from './Left';
 import Right from './Right';
 
 function Header() {
+    const [sliderData, setSliderData] = useState([]);
+
+    // get data
+    const getSliderData = async () => {
+        try {
+            const { data } = await axios.get('http://localhost:8080/slider-data');
+            if (data.success) {
+                setSliderData(data.headerData.data);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error('Error Occured while fetching the Slider Data from Server!!');
+        }
+    };
+
+    useEffect(() => {
+        getSliderData();
+    }, []);
+
+    console.log(sliderData);
     return (
         <div className="hero min-h-screen" style={{ backgroundImage: `url(${bannerImg})` }}>
             <div className="hero-overlay bg-opacity-30" />
             <div className="relative z-10">
                 <div className="grid grid-cols-12 justify-items-center px-12 items-center h-full mt-12">
-                    <div className="col-span-3">
+                    <div className="col-span-4">
                         <Left />
                     </div>
-                    <div className="col-span-9 ml-24">
-                        <Right />
+                    <div className="col-span-8 ml-24">
+                        <Right data={sliderData} />
                     </div>
                 </div>
             </div>
--- src/components/pages/HomePageContent/Right.jsx
+++ src/components/pages/HomePageContent/Right.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable comma-dangle */
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable react/jsx-indent */
+import React, { useState } from 'react';
+// import Swiper core and required modules
+import { EffectCoverflow } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
 
-function Right() {
-    return <div>Right</div>;
+function Right({ data }) {
+    const [indexes, setIndex] = useState(0);
+
+    function handleSlideChange(swiper) {
+        setIndex(swiper.realIndex);
+    }
+
+    return (
+        <Swiper
+            className="max-w-4xl"
+            onSlideChange={handleSlideChange}
+            modules={[EffectCoverflow]}
+            effect="coverflow"
+            grabCursor
+            centeredSlides
+            slidesPerView={6}
+            coverflowEffect={{
+                rotate: 0,
+                stretch: 20,
+                depth: 90,
+                modifier: 1,
+                slideShadows: true
+            }}>
+            <SwiperSlide />
+            {data.map((el) => (
+                <SwiperSlide key={el.id}>
+                    <div className={`w-80 rounded-lg h-96  shadow-xl image-full relative ${indexes === el.id ? 'border-2 border-yellow-400' : 'border-0'}`}>
+                        <img src={el.img} className="w-full h-full rounded-lg object-cover" alt="Shoes" />
+                        <h1 className="hover-stroke absolute -top-20 right-0 text-4xl w-full font-bold" data-text={el.title}>
+                            {el.title}
+                        </h1>
+                    </div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    );
 }
 
 export default Right;
