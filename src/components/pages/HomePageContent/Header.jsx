d4288ab2c8ec0f2c339e07c36d1844471dc7c1b8
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-01 23:50:30 +0600

    done the header part

--- src/components/pages/HomePageContent/Header.jsx
+++ src/components/pages/HomePageContent/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-useless-fragment */
 /* eslint-disable import/extensions */
 /* eslint-disable max-len */
 /* eslint-disable react/style-prop-object */
@@ -41,19 +42,37 @@ function Header() {
 
     console.log(sliderData);
     return (
-        <div className="hero min-h-screen" style={{ backgroundImage: `url(${bannerImg})` }}>
-            <div className="hero-overlay bg-opacity-30" />
-            <div className="relative z-10">
-                <div className="grid grid-cols-12 justify-items-center px-12 items-center h-full mt-12">
-                    <div className="col-span-4">
-                        <Left data={backgroudImage} id={ids} />
-                    </div>
-                    <div className="col-span-8 ml-24">
-                        <Right data={sliderData} getData={getData} />
+        <>
+            {isImageBg && ids !== 0 ? (
+                <div className="hero min-h-screen" style={{ backgroundImage: `url(${backgroudImage.img})` }}>
+                    <div className="hero-overlay bg-opacity-30" />
+                    <div className="relative z-10">
+                        <div className="grid grid-cols-12 justify-items-center px-12 items-center h-full mt-12">
+                            <div className="col-span-4">
+                                <Left data={backgroudImage} id={ids} />
+                            </div>
+                            <div className="col-span-8 ml-24">
+                                <Right data={sliderData} getData={getData} />
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
+            ) : (
+                <div className="hero min-h-screen" style={{ backgroundImage: `url(${bannerImg})` }}>
+                    <div className="hero-overlay bg-opacity-30" />
+                    <div className="relative z-10">
+                        <div className="grid grid-cols-12 justify-items-center px-12 items-center h-full mt-12">
+                            <div className="col-span-4">
+                                <Left data={backgroudImage} id={ids} />
+                            </div>
+                            <div className="col-span-8 ml-24">
+                                <Right data={sliderData} getData={getData} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
 
