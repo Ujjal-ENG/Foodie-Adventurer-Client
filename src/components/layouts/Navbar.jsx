6a28d8cf9c311b013b082adcfbcf4b877c2282dd
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-02 05:22:43 +0600

    almost done the recipe page

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -23,9 +23,15 @@ function Navbar() {
         <nav className={`z-50 relative flex justify-between items-center duration-200 transition-all ${isScrolled ? 'sticky top-0 bg-white shadow-md px-6 py-2 ease-in' : 'my-container ease-out'}`}>
             <h1 className="text-3xl font-bold">Foodie Adventure</h1>
             <div className="flex justify-around items-center gap-12 text-3xl">
-                <NavLink className={({ isAcitve }) => (isAcitve ? 'active' : 'default')}>Home</NavLink>
-                <NavLink className={({ isAcitve }) => (isAcitve ? 'active' : 'default')}>Blog</NavLink>
-                <NavLink className={({ isAcitve }) => (isAcitve ? 'active' : 'default')}>Login/Register</NavLink>
+                <NavLink to="/" className={({ isAcitve }) => (isAcitve ? 'active' : 'default')}>
+                    Home
+                </NavLink>
+                <NavLink to="/blog" className={({ isAcitve }) => (isAcitve ? 'active' : 'default')}>
+                    Blog
+                </NavLink>
+                <NavLink to="/login" className={({ isAcitve }) => (isAcitve ? 'active' : 'default')}>
+                    Login
+                </NavLink>
                 <div className="avatar">
                     <div className="w-12">
                         <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600" alt="avatar" className="rounded-full" />
--- src/components/pages/ChefRecipes/ChefRecipes.jsx
+++ src/components/pages/ChefRecipes/ChefRecipes.jsx
@@ -1,10 +1,38 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/jsx-indent */
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 function ChefRecipes() {
     const loader = useLoaderData();
-    console.log(loader);
-    return <div>ChefRecipes</div>;
+    const { findData } = loader;
+    return (
+        <div>
+            <div className="hero min-h-[calc(70vh)] bg-cover" style={{ backgroundImage: `url(${findData.chefPicture})` }}>
+                <div className="hero-overlay bg-opacity-70" />
+                <div className="hero-content flex-col lg:flex-row-reverse text-white">
+                    <img src={findData.chefPicture} className="max-w-sm rounded-lg shadow-2xl" alt={findData.chefName} />
+                    <div>
+                        <h1 className="text-5xl font-bold hover-stroke">{findData.chefName}</h1>
+                        <p className="py-6 tracking-wider text-xl font-bold">{findData.description}</p>
+                        <div className="flex justify-evenly items-center text-2xl font-bold">
+                            <p className="text-amber-500  mb-2">
+                                Years of experience: <span className="text-white">{findData.yearsOfExperience}</span>{' '}
+                            </p>
+                            <p className="text-amber-500">
+                                Number of recipes: <span className="text-white">{findData.numberOfRecipes}</span>{' '}
+                            </p>
+                            <p className="text-amber-500  ">
+                                Likes: <span className="text-white">{findData.likes}</span>{' '}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="my-container">receps</div>
+        </div>
+    );
 }
 
 export default ChefRecipes;
