eef45da2cb48735978b4777c0f7cb52ab90db058
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-02 21:24:41 +0600

    chnage some design in error page

--- src/components/pages/ErrorPage.jsx
+++ src/components/pages/ErrorPage.jsx
@@ -1,16 +1,22 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
+/* eslint-disable react/self-closing-comp */
 /* eslint-disable react/jsx-indent */
 import React from 'react';
-import { FaExclamationTriangle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function ErrorPage() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <div className="flex flex-col items-center justify-center">
-                <FaExclamationTriangle className="text-4xl text-red-500 mb-4" />
-                <h1 className="text-2xl font-bold mb-2">Oops, something went wrong!</h1>
-                <p className="text-lg text-center mb-4">We apologize for the inconvenience. Please try again later.</p>
-                <img src="https://images.pexels.com/photos/3760790/pexels-photo-3760790.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Error" className="w-80" />
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
+            <div className="max-w-lg mb-10 flex items-center justify-center">
+                <img src="https://picsum.photos/id/237/400/400" alt="404 Error" />
+                <iframe src="https://embed.lottiefiles.com/animation/98642" className="max-w-lg w-full"></iframe>
             </div>
+
+            <h1 className="text-4xl font-bold text-gray-800 mb-4">Oops! Page not found.</h1>
+            <p className="text-lg text-gray-800 mb-8">We're sorry, the page you're looking for does not exist.</p>
+            <Link to="/" className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-md transition-colors duration-200">
+                Go back to homepage
+            </Link>
         </div>
     );
 }
