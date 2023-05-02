f7c836766f2e74ebe14cc5cdbf097ab49a2d4627
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-02 17:49:03 +0600

    added some framer motion animation

--- src/components/pages/HomePageContent/Left.jsx
+++ src/components/pages/HomePageContent/Left.jsx
@@ -3,28 +3,33 @@
 /* eslint-disable react/jsx-closing-bracket-location */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/jsx-indent */
+import { motion } from 'framer-motion';
 import React from 'react';
 
 function Left({ data, id }) {
     if (data === null || id === 0) {
         return (
-            <div className="px-4">
+            <motion.div
+                className="flex flex-col  items-center lg:items-start mb-10 lg:mb-0"
+                initial={{ opacity: 0, x: -150 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.9, delay: 0.3 }}>
                 <span
                     className="hover-stroke text-4xl w-full font-bold"
                     data-text="Food is not just fuel, it's information. It talks to your DNA and tells it what to do, how to behave, and whether to express or suppress certain genes. So, eat the best and make your body sing with joy!">
                     Food is not just fuel, it's information. It talks to your DNA and tells it what to do, how to behave, and whether to express or suppress certain genes. So, eat the best and make
                     your body sing with joy!
                 </span>
-            </div>
+            </motion.div>
         );
     }
     return (
-        <div className="w-full space-y-4">
+        <motion.dev initial={{ opacity: 0, x: -150 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.9, delay: 0.3 }} className="w-full space-y-4">
             <h1 className="hover-stroke uppercase tracking-widest text-5xl font-bold " data-text={data.title}>
                 {data.title}
             </h1>
             <p className="text-gray-100 font-bold text-xl tracking-wider ">{`${data.description}`}</p>
-        </div>
+        </motion.dev>
     );
 }
 
