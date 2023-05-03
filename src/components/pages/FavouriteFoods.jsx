72ec5ca6898332f89e43891fe915ea4fb224398b
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-03 17:49:14 +0600

    now the favourite food is fully responsive and full customizable

--- src/components/pages/FavouriteFoods.jsx
+++ src/components/pages/FavouriteFoods.jsx
@@ -44,44 +44,48 @@ function FavouriteFoods() {
 
     return (
         <div>
-            <div className="overflow-x-auto max-w-7xl mx-auto">
-                <table className="table w-full">
-                    {/* head */}
-                    <thead>
-                        <tr>
-                            <th />
-                            <th>Food Name</th>
-                            <th>Food Photo</th>
-                            <th>Food Chef Name</th>
-                            <th>Food Chef Photo</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {/* row 1 */}
+            {getLocalStorageFood.length === 0 ? (
+                <h1 className="my-container text-3xl md:text-7xl font-bold title-text text-center"> Yor Do not Added and Favorite Food from the Recipe Section!!!</h1>
+            ) : (
+                <div className="overflow-x-auto max-w-7xl mx-auto">
+                    <table className="table w-full">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th />
+                                <th>Food Name</th>
+                                <th>Food Photo</th>
+                                <th>Food Chef Name</th>
+                                <th>Food Chef Photo</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
 
-                        {getLocalStorageFood &&
-                            getLocalStorageFood.map((food) => (
-                                <tr key={count}>
-                                    <th>{count++}</th>
-                                    <td>
-                                        <img className="mask mask-square md:w-48 w-20" src={food.img} alt={food.recipeName} />
-                                        {/* <img src={food.img} alt={food.recipeName} /> */}
-                                    </td>
-                                    <td className="text-xl md:text-2xl font-bold">{food.recipeName}</td>
-                                    <td className="text-xl md:text-2xl font-bold"> {food.chefName}</td>
-                                    <td>
-                                        {/* <img src={food.chefPicture} alt={food.chefName} /> */}
-                                        <img className="mask mask-circle md:w-48 w-20 " src={food.chefPicture} alt={food.chefName} />
-                                    </td>
-                                    <td>
-                                        <AiFillDelete className="text-5xl text-red-500 cursor-pointer" onClick={() => handleDeleteFood(food.id, food.recipeName)} />
-                                    </td>
-                                </tr>
-                            ))}
-                    </tbody>
-                </table>
-            </div>
+                            {getLocalStorageFood &&
+                                getLocalStorageFood.map((food) => (
+                                    <tr key={count}>
+                                        <th>{count++}</th>
+                                        <td>
+                                            <img className="mask mask-square md:w-48 w-20" src={food.img} alt={food.recipeName} />
+                                            {/* <img src={food.img} alt={food.recipeName} /> */}
+                                        </td>
+                                        <td className="text-xl md:text-2xl font-bold">{food.recipeName}</td>
+                                        <td className="text-xl md:text-2xl font-bold"> {food.chefName}</td>
+                                        <td>
+                                            {/* <img src={food.chefPicture} alt={food.chefName} /> */}
+                                            <img className="mask mask-circle md:w-48 w-20 " src={food.chefPicture} alt={food.chefName} />
+                                        </td>
+                                        <td>
+                                            <AiFillDelete className="text-5xl text-red-500 cursor-pointer" onClick={() => handleDeleteFood(food.id, food.recipeName)} />
+                                        </td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
         </div>
     );
 }
