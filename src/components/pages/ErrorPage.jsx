777b4c578f3700dcf0f3b7a7b9c2a430dce7b1ae
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-03 17:59:31 +0600

    now the page is fully responsive

--- src/components/pages/ErrorPage.jsx
+++ src/components/pages/ErrorPage.jsx
@@ -8,13 +8,13 @@ import { Link } from 'react-router-dom';
 function ErrorPage() {
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
-            <div className="max-w-lg mb-10 flex items-center justify-center">
-                <img src="https://picsum.photos/id/237/400/400" alt="404 Error" />
-                <iframe src="https://embed.lottiefiles.com/animation/98642" className="max-w-lg w-full"></iframe>
+            <div className="max-w-lg mb-10 flex flex-col items-center justify-center">
+                <img src="https://picsum.photos/id/237/400/400" alt="404 Error" className="w-full mb-6 md:max-w-none md:mr-6 md:mb-0" />
+                <iframe src="https://embed.lottiefiles.com/animation/98642" className="max-w-lg w-full md:w-auto md:max-w-none"></iframe>
             </div>
 
-            <h1 className="text-4xl font-bold text-gray-800 mb-4">Oops! Page not found.</h1>
-            <p className="text-lg text-gray-800 mb-8">We're sorry, the page you're looking for does not exist.</p>
+            <h1 className="text-4xl font-bold text-gray-800 mb-4 text-center">Oops! Page not found.</h1>
+            <p className="text-lg text-gray-800 mb-8 text-center">We're sorry, the page you're looking for does not exist.</p>
             <Link to="/" className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-md transition-colors duration-200">
                 Go back to homepage
             </Link>
